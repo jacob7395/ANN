7433c90a3afec2d3a3ed2dc2b39ae885ec4df717
Jacob <jacob7395@users.noreply.github.com>
2019-05-31 14:53:37 +0100

    Fixed Multi threading
    Update to fix issues caused by multi threading.

--- Tournament.py
+++ Tournament.py
@@ -8,9 +8,11 @@ import numpy as np
 from PythonNetwork import NeuralNetwork
 from TicTacToe import TicTacToe
 
+import matplotlib.pyplot as plt
+
 MUTRATION_RATE = 1
 MOVE_BONUS = 15
-GEN_SIZE = 2 ** 10
+GEN_SIZE = 2 ** 15
 
 
 class player:
@@ -62,7 +64,7 @@ class match:
 
 class tournament:
 
-    def __init__(self, networks, game, game_queue, complete_game_queue):
+    def __init__(self, networks, game, game_queue, complete_game_queue, worker_load = 5):
 
         self.players = []
 
@@ -73,43 +75,68 @@ class tournament:
         self.waiting_players = self.players
         self.game = game
         self.match_list = []
-        self.worker_load = 5
+        self.worker_load = worker_load
         self.game_queue = game_queue
         self.complete_games = []
         self.complete_game_queue = complete_game_queue
+        self.tournament_run_time = 0
 
     def play(self):
 
+        start_time = time.time()
         while self.games_left != 0:
-            self.setup_games()
+            self.match_maker()
 
             self.fill_queue()
 
             finished_games = []
             try:
-                finished_games = self.complete_game_queue.get(False)
+                while True:
+                    finished_games += self.complete_game_queue.get(False)
             except:
                 pass
+ 
+            self.games_left -= len(finished_games)
 
             for game in finished_games:
                 self.waiting_players.append(game.winner)
                 self.complete_games.append(game)
-                self.games_left -= 1
+        self.tournament_run_time = time.time() - start_time
+                
 
-    def setup_games(self):
+    def match_maker(self):
 
-        while len(self.waiting_players) > 1:
+        match_failed = []
+        while len(self.waiting_players) > 0:
+            new_match = None
             player1 = self.waiting_players.pop(0)
-            player2 = self.waiting_players.pop(0)
-            self.match_list.append(match(players=[player1, player2], game=game))
+            for player2 in self.waiting_players:
+                if player2.round == player1.round:
+                    self.waiting_players.remove(player2)
+                    new_match = match(players=[player1, player2], game=game)
+                    self.match_list.append(new_match)
+                    break
+            
+            if new_match == None:
+                match_failed.append(player1)
+        
+        self.waiting_players = match_failed
 
     def fill_queue(self):
 
-        while len(self.match_list) >= self.worker_load or (
-                self.games_left <= self.worker_load and self.match_list != []):
-            match_slice = self.match_list[:self.worker_load]
+        if len(self.match_list) < self.worker_load:
+            load = max(self.game_queue.qsize(), 1) if self.game_queue.qsize() < self.worker_load else self.worker_load
+        else:
+            load = self.worker_load
+
+        while len(self.match_list) >= load:
+            match_slice = self.match_list[:load]
             self.game_queue.put(match_slice)
-            del self.match_list[:self.worker_load]
+            del self.match_list[:load]
+
+    def stats(self):
+
+        pass
 
 
 def work_game(game_queue, complete_game_queue):
@@ -161,13 +188,20 @@ if __name__ == "__main__":
 
     players = base_network.spawn(player_count)
 
-    for gen_count in range(50000):
+    # running load test
+    timing_data = []
+    for gen_count in range(50):
         np.random.shuffle(players)
 
-        torney = tournament(players, game, game_queue, complete_game_queue)
+        torney = tournament(networks = players, game = game, game_queue = game_queue, complete_game_queue = complete_game_queue, worker_load = 13)
         torney.play()
 
-        os.system('clear')
-        print(torney.complete_games[-1].game)
+        timing_data.append(torney.tournament_run_time)
+
         print("Generation {}".format(gen_count))
-        time.sleep(0.1)
+
+    plt.boxplot(timing_data)
+    plt.show()
+
+    for worker in workerforce:
+        worker.terminate()
